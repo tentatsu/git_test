9bfe04d0ce53d00fede17d3ea0557197dd3959af
Hyodo <t_hyodo@enfactory.co.jp>
2014-02-10 17:17:31 +0900

    チャーリー追加。

--- hyo-sagyou-1.php
+++ hyo-sagyou-1.php
@@ -3,4 +3,5 @@ hyo-sagyou-1ブランチで追加したファイル。
 echo "Jacky:hanage dete masuyo <br>";
 echo "Sudan:lady ni mukatte shiturei ne!!!!<br>";
 echo "Danny:Hey Baby I`m Daniel<br>";
+echo "Charry:Chary is come.Hey!Jule!!<br>"
 
